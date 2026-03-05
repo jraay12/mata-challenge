07eef6df5984a5e9fe8ae6f5da48bdc5cac8e3e9
John Ray <johnraycaete@gmail.com>
2026-03-05 16:48:27 +0800

    feat(user): implement prisma user repository

---
+++ src/infrastructure/repositories/UserRepository.ts
@@ -0,0 +1,41 @@
+import { User } from "../../domain/entities/User";
+import { IUserRepository } from "../../domain/repositories/IUserRepository";
+import { PrismaClient } from "@prisma/client";
+import { Role as DomainRole } from "../../domain/entities/value-objects/Roles";
+export class UserRepository implements IUserRepository {
+  constructor(private prisma: PrismaClient) {}
+
+  async create(user: User): Promise<void> {
+    await this.prisma.user.create({
+      data: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        password: user.password,
+        role: user.role,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+      },
+    });
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) return null;
+
+    return User.fromPersistence({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      role: user.role === "STAFF" ? DomainRole.STAFF : DomainRole.ADMIN,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    });
+  }
+}
