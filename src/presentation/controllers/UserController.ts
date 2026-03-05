44a442f96e5025f91ddfd5af9dd4c6abcdc33322
John Ray <johnraycaete@gmail.com>
2026-03-06 00:32:40 +0800

    feat(controller): add login method to UserController using LoginUserUsecase

--- src/presentation/controllers/UserController.ts
+++ src/presentation/controllers/UserController.ts
@@ -1,9 +1,17 @@
 import { CreateUserUsecase } from "../../application/usecase/CreateUserUsecase";
-import { CreateUserSchema, CreateUserDTO } from "../../application/dto/CreateUserDTO";
+import {
+  CreateUserSchema,
+  CreateUserDTO,
+} from "../../application/dto/CreateUserDTO";
 import { FastifyReply, FastifyRequest } from "fastify";
+import { LoginUserUsecase } from "../../application/usecase/LoginUserUsecase";
+import { LoginSchema, LoginUserDTO } from "../../application/dto/LoginUserDTO";
 
 export class UserController {
-  constructor(private createUserUsecase: CreateUserUsecase) {}
+  constructor(
+    private createUserUsecase: CreateUserUsecase,
+    private loginUserUsecase: LoginUserUsecase,
+  ) {}
 
   createUser = async (
     request: FastifyRequest,
@@ -16,7 +24,32 @@ export class UserController {
 
       reply.code(201).send(user);
     } catch (error) {
-      throw error; 
+      throw error;
+    }
+  };
+
+  login = async (
+    request: FastifyRequest,
+    reply: FastifyReply,
+  ): Promise<void> => {
+    try {
+      const dto: LoginUserDTO = LoginSchema.parse(request.body);
+
+      const { refreshToken, accessToken } =
+        await this.loginUserUsecase.execute(dto);
+      reply.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        path: "/",
+        signed: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 60,
+      });
+      reply.code(200).send({
+        token: accessToken
+      });
+    } catch (error) {
+      throw error;
     }
   };
 }
