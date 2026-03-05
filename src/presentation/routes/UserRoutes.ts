f48010a576972af5f3f329a3c7509263a1d76151
John Ray <johnraycaete@gmail.com>
2026-03-06 00:33:44 +0800

    feat(routes): add login route to userRoutes

--- src/presentation/routes/UserRoutes.ts
+++ src/presentation/routes/UserRoutes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 export async function userRoutes(fastify: FastifyInstance) {
-
   fastify.post("/create", (req, reply) =>
     fastify.userController.createUser(req, reply),
   );
-  
+
+  fastify.post("/login", (req, reply) =>
+    fastify.userController.login(req, reply),
+  );
 }
