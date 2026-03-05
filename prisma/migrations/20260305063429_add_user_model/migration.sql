a5d6eb824d1890a425ccf6b56ab171cb5d599c3b
John Ray <johnraycaete@gmail.com>
2026-03-05 14:35:06 +0800

    chore: add Prisma migration for User model

---
+++ prisma/migrations/20260305063429_add_user_model/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- DropForeignKey
+ALTER TABLE `Post` DROP FOREIGN KEY `Post_authorId_fkey`;
+
+-- AlterTable
+ALTER TABLE `User` DROP PRIMARY KEY,
+    ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `password` VARCHAR(191) NOT NULL,
+    ADD COLUMN `role` ENUM('STAFF', 'ADMIN') NOT NULL DEFAULT 'STAFF',
+    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL,
+    MODIFY `id` VARCHAR(191) NOT NULL,
+    MODIFY `name` VARCHAR(191) NOT NULL,
+    ADD PRIMARY KEY (`id`);
+
+-- DropTable
+DROP TABLE `Post`;
