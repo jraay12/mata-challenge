51423567d231160548bf5d221d9d58182c463bca
John Ray <johnraycaete@gmail.com>
2026-03-06 03:16:55 +0800

    feat: extend IUserRepository with findByID and updateDetails methods

--- src/domain/repositories/IUserRepository.ts
+++ src/domain/repositories/IUserRepository.ts
@@ -2,5 +2,7 @@ import { User } from "domain/entities/User";
 
 export interface IUserRepository {
   create(user: User): Promise<void>;
-  findByEmail(email: string): Promise<User | null>
+  findByEmail(email: string): Promise<User | null>;
+  findByID(id: string): Promise<User | null>;
+  updateDetails(user: User): Promise<void>;
 }
