2cc68e89a1e741a62baea7399b1f9962d9804c88
John Ray <johnraycaete@gmail.com>
2026-03-05 16:53:07 +0800

    chore(app): register routes and error handler in fastify app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import Fastify from "fastify";
-
+import { userRoutes } from "./presentation/routes/UserRoutes";
+import { registerErrorHandler } from "./infrastructure/errors/errorHandler";
 export function buildApp() {
   const app = Fastify({ logger: true });
+  registerErrorHandler(app);
 
   // checker if server is running
   app.get("/health", async () => {
@@ -11,6 +13,8 @@ export function buildApp() {
       timestamp: new Date().toISOString(),
     };
   });
+
+  app.register(userRoutes, { prefix: "/user" });
 
   return app;
 }
