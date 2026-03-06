34e07d5408443f3cb0bfe34acc6a9fac896c0b5c
John Ray <johnraycaete@gmail.com>
2026-03-06 12:07:50 +0800

    feat(routes): add GET /products endpoint

--- src/presentation/routes/ProductRoutes.ts
+++ src/presentation/routes/ProductRoutes.ts
@@ -1,6 +1,15 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
+import { GetAllProductsQuery } from "../../application/dto/GetAllProductQuery";
 export async function productRoutes(fastify: FastifyInstance) {
   fastify.post("/create-product", (req, reply) =>
     fastify.productController.create(req, reply),
+  );
+
+  fastify.get(
+    "/products",
+    (
+      req: FastifyRequest<{ Querystring: GetAllProductsQuery }>,
+      reply: FastifyReply,
+    ) => fastify.productController.getProduct(req, reply),
   );
 }
