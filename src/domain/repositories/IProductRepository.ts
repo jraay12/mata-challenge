e5d72960527a21006cc7f7e56d8372a145ef1679
John Ray <johnraycaete@gmail.com>
2026-03-06 13:17:24 +0800

    feat(product-repository): add stock update and findById methods

--- src/domain/repositories/IProductRepository.ts
+++ src/domain/repositories/IProductRepository.ts
@@ -2,4 +2,6 @@ import { Product } from "./../entities/Product";
 export interface IProductRepository {
   create(product: Product): Promise<void>;
   getAllProduct(page?: number, limit?: number): Promise<Product[]>;
+  addStock(product_id: string, stock: number): Promise<void>;
+  findById(product_id: string): Promise<Product | null>;
 }
