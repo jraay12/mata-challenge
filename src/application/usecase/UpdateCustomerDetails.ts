564bda05046449018a1fef5749bbceda5ec11221
John Ray <johnraycaete@gmail.com>
2026-03-06 09:51:27 +0800

    fix: throw NotFoundError instead of generic Error in Update Details Usecase

--- src/application/usecase/UpdateCustomerDetails.ts
+++ src/application/usecase/UpdateCustomerDetails.ts
@@ -1,13 +1,14 @@
 import { UpdateCustomerDTO } from "application/dto/UpdateCustomerDTO";
 import { UserRepository } from "infrastructure/repositories/UserRepository";
-import { User } from "domain/entities/User";
+import { User } from "../../domain/entities/User";
+import { NotFoundError } from "../../domain/errors/NotFoundError";
 export class UpdateCustomerDetails {
   constructor(private userRepo: UserRepository) {}
 
   async execute(id: string, data: UpdateCustomerDTO): Promise<User> {
     const user = await this.userRepo.findByID(id);
 
-    if (!user) throw new Error("User not found");
+    if (!user) throw new NotFoundError("User not found");
 
     user.updateFields({
       address: data.address,
