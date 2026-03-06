18790327c6249a3a89190cd1b53f11c5d387687f
John Ray <johnraycaete@gmail.com>
2026-03-06 11:24:16 +0800

    feat(product): add Product entity

---
+++ src/domain/entities/Product.ts
@@ -0,0 +1,83 @@
+import crypto from "crypto";
+import { BadRequestError } from "../../domain/errors/BadRequestError";
+import { ProductResponseDTO } from "../../application/dto/ProductResponseDTO";
+export interface ProductProps {
+  id: string;
+  name: string;
+  price: number;
+  stock?: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export class Product {
+  private props: ProductProps;
+  private static readonly MAX_NAME_LENGTH = 100;
+
+  constructor(props: ProductProps) {
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+      updatedAt: props.updatedAt ?? new Date(),
+    };
+  }
+
+  static create(props: Omit<ProductProps, "id" | "createdAt" | "updatedAt">) {
+    if (!props.name || props.name.trim().length === 0) {
+      throw new BadRequestError("Product name is required");
+    }
+    if (props.name.length > Product.MAX_NAME_LENGTH) {
+      throw new BadRequestError(
+        `Product name cannot exceed ${Product.MAX_NAME_LENGTH} characters`,
+      );
+    }
+
+    if (props.price <= 0) {
+      throw new BadRequestError("Price cannot be negative or zero");
+    }
+
+    if (props.stock !== undefined && props.stock < 0) {
+      throw new BadRequestError("Stock cannot be negative");
+    }
+
+    return new Product({
+      ...props,
+      id: crypto.randomUUID(),
+    });
+  }
+
+  static fromPersistenceArray(data: ProductProps[]): Product[] {
+    return data.map((product) => new Product(product));
+  }
+
+  toJSON(): ProductResponseDTO {
+    return {
+      id: this.id,
+      name: this.name,
+      price: this.price,
+      stock: this.stock,
+      createdAt: this.createdAt,
+      updatedAt: this.updatedAt,
+    };
+  }
+
+  // Getters
+  get id() {
+    return this.props.id;
+  }
+  get name() {
+    return this.props.name;
+  }
+  get price() {
+    return this.props.price;
+  }
+  get stock() {
+    return this.props.stock ?? 0;
+  }
+  get createdAt() {
+    return this.props.createdAt!;
+  }
+  get updatedAt() {
+    return this.props.updatedAt!;
+  }
+}
