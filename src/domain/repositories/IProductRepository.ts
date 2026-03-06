c40a5e147d126baf6b344a1ec964ef19c8fb992c
John Ray <johnraycaete@gmail.com>
2026-03-06 12:05:40 +0800

    feat(product): add getAllProduct method to IProductRepository

--- src/domain/repositories/IProductRepository.ts
+++ src/domain/repositories/IProductRepository.ts
@@ -1,4 +1,5 @@
 import { Product } from "./../entities/Product";
 export interface IProductRepository {
   create(product: Product): Promise<void>;
+  getAllProduct(page?: number, limit?: number): Promise<Product[]>;
 }
