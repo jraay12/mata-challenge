ca7323b43e4838de47ecbd60d5d753d5aeeae8d3
John Ray <johnraycaete@gmail.com>
2026-03-06 12:07:02 +0800

    feat(controller): add getProduct endpoint to ProductController

--- src/presentation/controllers/ProductController.ts
+++ src/presentation/controllers/ProductController.ts
@@ -1,12 +1,17 @@
+import { GetAllProductUsecase } from "../../application/usecase/GetAllProductUsecase";
 import {
   CreateProductSchema,
   CreateProductDTO,
 } from "../../application/dto/CreateProductDTO";
 import { CreateProductUsecase } from "../../application/usecase/CreateProductUsecase";
 import { FastifyReply, FastifyRequest } from "fastify";
+import { GetAllProductsQuery } from "../../application/dto/GetAllProductQuery";
 
 export class ProductController {
-  constructor(private createProduct: CreateProductUsecase) {}
+  constructor(
+    private createProduct: CreateProductUsecase,
+    private getAllProductUsecase: GetAllProductUsecase,
+  ) {}
 
   create = async (
     request: FastifyRequest,
@@ -15,5 +20,17 @@ export class ProductController {
     const dto: CreateProductDTO = CreateProductSchema.parse(request.body);
     const product = await this.createProduct.execute(dto);
     reply.code(201).send({ message: "Create Product Succesfully", product });
+  };
+
+  getProduct = async (
+    request: FastifyRequest<{ Querystring: GetAllProductsQuery }>,
+    reply: FastifyReply,
+  ): Promise<void> => {
+    const page = Number(request.query?.page) || 1;
+    const limit = Number(request.query?.limit) || 10;
+    const products = await this.getAllProductUsecase.execute(page, limit);
+    reply
+      .code(201)
+      .send({ message: "Retrieve Products Succesfully", products });
   };
 }
